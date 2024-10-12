9bce4ceb7e51c20358038f7642ecb5bda69a58c7
andrzejkukuryk <101364440+andrzejkukuryk@users.noreply.github.com>
2024-10-12 10:17:30 +0200

    add results components

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -1,15 +1,30 @@
 import { useEffect } from "react";
 import { useMemoryStore } from "../../store";
+import { formatDateToString } from "../../utils/formatDateToString";
+import { formatTime } from "../../utils/formatTime";
+import { Level } from "../../constants/levels.enum";
+import { ResultsEasy } from "../resultsEasy/resultsEasy";
+import { ResultsNormal } from "../resultsNormal/resultsNormal";
+import { ResultsHard } from "../resultsHard/resultsHard";
 
 export const Result = () => {
-  const { resultsEasy, addEasy, setLs } = useMemoryStore();
+  const {
+    level,
+    movesCounter,
+    time,
+    resultsEasy,
+    resultsNormal,
+    resultsHard,
+    addEasy,
+    setLs,
+  } = useMemoryStore();
 
   useEffect(() => {
-    console.log(resultsEasy);
+    console.log(resultsEasy[0].date);
   }, [resultsEasy]);
 
   const kopytko = () => {
-    addEasy({ moves: 3, time: 432, date: new Date(Date.now()) });
+    addEasy({ moves: movesCounter, time: time, date: new Date(Date.now()) });
   };
 
   const save = () => {
@@ -19,6 +34,9 @@ export const Result = () => {
     <div>
       <button onClick={kopytko}> add easy</button>
       <button onClick={save}> save</button>
+      {level === Level.easy && <ResultsEasy />}
+      {level === Level.normal && <ResultsNormal />}
+      {level === Level.hard && <ResultsHard />}
     </div>
   );
 };
